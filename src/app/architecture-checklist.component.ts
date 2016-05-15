44053da90e238589f8d1decb413be707e4810d06
og84 <c.goetze@gmx.eu>
2016-05-15 10:44:53 +0200

    add header and footer components

--- src/app/architecture-checklist.component.ts
+++ src/app/architecture-checklist.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   moduleId: module.id,
   selector: 'architecture-checklist-app',
   templateUrl: 'architecture-checklist.component.html',
-  styleUrls: ['architecture-checklist.component.css']
+  styleUrls: ['architecture-checklist.component.css'],
+  directives: [HeaderComponent, FooterComponent]
 })
 export class ArchitectureChecklistAppComponent {
   title = 'architecture-checklist works!';
--- src/system-config.ts
+++ src/system-config.ts
@@ -29,6 +29,8 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/header',
+  'app/footer',
   /** @cli-barrel */
 ];
 
